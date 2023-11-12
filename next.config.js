f3f1da3993597b6da1d1857cc7f59713648115da
Nittin Shankar <nittin@Nittins-Macbook-Pro.local>
2023-11-12 12:24:11 +0530

    Fix CSP for Plausible

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self' *.plausible.io;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app plausible.io;
   style-src 'self' 'unsafe-inline' giscus.app;
   img-src * blob: data:;
   media-src 'none';
