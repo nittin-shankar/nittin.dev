d4e63fbf0af57345d469e8190f561fff715a685b
Nittin Shankar <nittin@Nittins-Macbook-Pro.local>
2023-11-12 11:47:13 +0530

    Add a button that links to my IG account

--- pages/podcast.js
+++ pages/podcast.js
@@ -52,6 +52,23 @@ export default function Podcasts(_) {
         </a>
 
         <a
+          href="https://instagram.com/nittin.shankar"
+          className="flex-inline mx-auto flex w-72 items-center justify-center gap-2 rounded border py-2 text-xs uppercase dark:border dark:border-gray-500 dark:text-white"
+        >
+          <Image
+            alt="instagram logo"
+            src="/static/images/instagram-logo.png"
+            width={26}
+            height={26}
+          />
+
+          <div className="">
+            <div className="m-0 p-0 text-[0.6rem]">Follow me on</div>
+            <div className="font-semibold">Instagram</div>
+          </div>
+        </a>
+
+        <a
           href="https://podcasts.apple.com/us/podcast/vaanga-pazhagalaam-with-techies/id1715171723"
           className="flex-inline my-2 mx-auto hidden w-72 items-center justify-center rounded border border border-gray-500 py-2.5 dark:flex"
         >
