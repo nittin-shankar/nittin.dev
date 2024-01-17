0e187a8e6c5a4b4403f82d6eab35c3127acf12c0
Narandra Sridar <nittin.shankar@gmail.com>
2024-01-17 11:15:36 +0530

    Add a separate page for meetup similar to podcast that redirects to a different URL

---
+++ pages/meetup.js
@@ -0,0 +1,26 @@
+import Link from '@/components/Link'
+
+export async function getStaticProps() {
+  return { props: { isMeetupPage: true } }
+}
+
+export default function Meetup(_) {
+  return (
+    <div className="mx-auto mt-10 max-w-lg text-center">
+      <p>Redirecting to the page of the meetup...</p>
+      <p>
+        Please click this
+        <Link
+          className="pt-8 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          href={
+            'https://kommunity.com/passionate-techies-meetup-group-py-av/events/meetup-1-b03bc360'
+          }
+        >
+          {' '}
+          link{' '}
+        </Link>{' '}
+        if it doesn't automatically redirect
+      </p>
+    </div>
+  )
+}
